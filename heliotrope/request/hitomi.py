9adb9185ad398dbb2c74dd776b08cfe3136e7389
Ryu JuHeon <saidbysolo@gmail.com>
2021-09-26 13:35:08 +0900

    fix(request/hitomi): pop first

--- heliotrope/request/hitomi.py
+++ heliotrope/request/hitomi.py
@@ -37,8 +37,8 @@ class HitomiRequest(BaseRequest):
 
     @classmethod
     async def setup(cls, **kwargs: Any) -> "HitomiRequest":
-        session = ClientSession(**kwargs)
         index_file = kwargs.pop("INDEX_FILE", "index-korean.nozomi")
+        session = ClientSession(**kwargs)
         hitomi_request = cls(session, index_file)
         hitomi_request.session.headers.update(hitomi_request.headers)
         return hitomi_request
