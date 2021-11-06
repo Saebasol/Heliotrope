b5f1d5ecd0d9b5ae65e3b849859c91ff6aeeb9da
Ryu JuHeon <saidbysolo@gmail.com>
2021-11-06 19:37:41 +0900

    feat(domain): add hitomi domains

---
+++ heliotrope/domain/file.py
@@ -0,0 +1,51 @@
+from typing import Literal, Optional
+from heliotrope.shared.domain_model import DomainModel
+from heliotrope.types import HitomiFileJSON
+
+
+class HitomiFile(DomainModel):
+    def __init__(
+        self,
+        width: int,
+        hash: str,
+        haswebp: Literal[0, 1],
+        name: str,
+        height: int,
+        hasavif: Optional[Literal[1]] = None,
+        hasavifsmalltn: Optional[Literal[1]] = None,
+    ) -> None:
+        self.width = width
+        self.hash = hash
+        self.haswebp: Literal[0, 1] = haswebp
+        self.name = name
+        self.height = height
+        self.hasavif: Optional[Literal[1]] = hasavif
+        self.hasavifsmalltn: Optional[Literal[1]] = hasavifsmalltn
+
+    @classmethod
+    def from_dict(cls, d: HitomiFileJSON) -> "HitomiFile":
+        return cls(
+            width=d["width"],
+            hash=d["hash"],
+            haswebp=d["haswebp"],
+            name=d["name"],
+            height=d["height"],
+            hasavif=d.get("hasavif"),
+            hasavifsmalltn=d.get("hasavifsmalltn"),
+        )
+
+    def to_dict(self) -> HitomiFileJSON:
+        hitomi_file_json = HitomiFileJSON(
+            width=self.width,
+            hash=self.hash,
+            haswebp=self.haswebp,
+            name=self.name,
+            height=self.height,
+        )
+        if self.hasavif is not None:
+            hitomi_file_json["hasavif"] = self.hasavif
+
+        if self.hasavifsmalltn is not None:
+            hitomi_file_json["hasavifsmalltn"] = self.hasavifsmalltn
+
+        return hitomi_file_json
---
+++ heliotrope/domain/galleryinfo.py
@@ -0,0 +1,56 @@
+from typing import Optional
+from heliotrope.domain.file import HitomiFile
+from heliotrope.domain.tag import HitomiTag
+from heliotrope.shared.domain_model import DomainModel
+from heliotrope.types import HitomiFileJSON, HitomiGalleryinfoJSON, HitomiTagJSON
+
+
+class HitomiGalleryInfo(DomainModel):
+    def __init__(
+        self,
+        title: str,
+        date: str,
+        type: str,
+        japanese_title: Optional[str],
+        language: str,
+        files: list[HitomiFileJSON],
+        id: str,
+        language_localname: str,
+        tags: list[HitomiTagJSON],
+    ) -> None:
+        self.title = title
+        self.date = date
+        self.type = type
+        self.japanese_title = japanese_title
+        self.language = language
+        self.files: list[HitomiFile] = [HitomiFile.from_dict(file) for file in files]
+        self.id = id
+        self.language_localname = language_localname
+        self.tags: list[HitomiTag] = [HitomiTag.from_dict(tag) for tag in tags]
+
+    @classmethod
+    def from_dict(cls, d: HitomiGalleryinfoJSON) -> "HitomiGalleryInfo":
+        return cls(
+            title=d["title"],
+            date=d["date"],
+            type=d["type"],
+            japanese_title=d["japanese_title"],
+            language=d["language"],
+            files=d["files"],
+            id=d["id"],
+            language_localname=d["language_localname"],
+            tags=d["tags"],
+        )
+
+    def to_dict(self) -> HitomiGalleryinfoJSON:
+        return HitomiGalleryinfoJSON(
+            title=self.title,
+            date=self.date,
+            type=self.type,
+            japanese_title=self.japanese_title,
+            language=self.language,
+            files=[file.to_dict() for file in self.files],
+            id=self.id,
+            language_localname=self.language_localname,
+            tags=[tag.to_dict() for tag in self.tags],
+        )
