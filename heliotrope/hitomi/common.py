c88e237da7b02177502e7dd858058cc5a7c1eecc
Ryu JuHeon <saidbysolo@gmail.com>
2021-11-20 13:03:11 +0900

    fix(common): execute after finding directly without parsing

--- heliotrope/hitomi/common.py
+++ heliotrope/hitomi/common.py
@@ -1,12 +1,9 @@
-from typing import cast
+from io import StringIO
+from typing import Any, cast
+
+from js2py.evaljs import EvalJs  # type: ignore
 
 from heliotrope.typing import HitomiFilesJSON
-from heliotrope.utils.js import (
-    ExtendEvalJS,
-    get_parsed_functions_from_source,
-    make_js_program,
-    translate_tree,
-)
 
 
 class CommonJS:
@@ -20,12 +17,30 @@ class CommonJS:
         "rewrite_tn_paths",
     ]
 
+    def get_js_function(self, code: str, function_names: list[str]):
+        functions: list[str] = []
+        lines = StringIO(code).readlines()
+
+        finded = False
+
+        for func_name in function_names:
+            for line in lines:
+                if finded:
+                    functions.append(line)
+                    if line.startswith("}"):
+                        finded = False
+                        continue
+                if line.startswith(f"function {func_name}"):
+                    functions.append(line)
+                    finded = True
+                    continue
+        return "".join(functions)
+
     def __init__(self, common_js: str) -> None:
-        self.engine = ExtendEvalJS()
-        self.body = get_parsed_functions_from_source(common_js, self.FUNCTIONS)
-        tree = make_js_program(self.body)
-        pycode = translate_tree(tree, "")
-        self.engine.exec_pycode(pycode)
+        super().__init__()
+        self.common_js = self.get_js_function(common_js, self.FUNCTIONS)
+        self.engine: Any = EvalJs()
+        self.engine.execute(self.common_js)
 
     def rewrite_tn_paths(self, html: str) -> str:
         return cast(str, self.engine.rewrite_tn_paths(html))
