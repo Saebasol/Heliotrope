733a7d692442a0f5effb816442d0694ef99de540
Ryu juheon <saidbysolo@gmail.com>
2021-11-23 17:55:45 +0900

    refactor: structure in database (#77)

---
+++ heliotrope/database/orm/table/__init__.py
@@ -0,0 +1,5 @@
+from heliotrope.database.orm.table.file import file_table
+from heliotrope.database.orm.table.gallleryinfo import galleryinfo_table
+from heliotrope.database.orm.table.tag import tag_table
+
+__all__ = ["file_table", "tag_table", "galleryinfo_table"]
