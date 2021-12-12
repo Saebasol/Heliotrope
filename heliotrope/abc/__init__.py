48dbd8dd9f86600e5e0aa1580c65b3bd151c4de7
Ryu JuHeon <saidbysolo@gmail.com>
2021-12-12 19:34:05 +0900

    refactor: remove ``__init__`` imports

--- heliotrope/abc/__init__.py
+++ heliotrope/abc/__init__.py
@@ -21,7 +21,3 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
-from heliotrope.abc.database import AbstractInfoDatabase, AbstractGalleryinfoDatabase
-from heliotrope.abc.task import AbstractTask
-
-__all__ = ["AbstractGalleryinfoDatabase", "AbstractInfoDatabase", "AbstractTask"]
