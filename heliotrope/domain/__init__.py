7e51108df6893c19794085ab4073e616e22bbcc6
Ryu JuHeon <saidbysolo@gmail.com>
2021-12-12 19:28:48 +0900

    refactor: remove ``__init__`` imports

--- heliotrope/domain/__init__.py
+++ heliotrope/domain/__init__.py
@@ -21,9 +21,3 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
-from heliotrope.domain.file import File
-from heliotrope.domain.galleryinfo import Galleryinfo
-from heliotrope.domain.info import Info
-from heliotrope.domain.tag import Tag
-
-__all__ = ["File", "Galleryinfo", "Tag", "Info"]
