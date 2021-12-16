306fad7517cbd807e7a2bd05fc6c6cbc18605f3e
Ryu JuHeon <saidbysolo@gmail.com>
2021-12-16 14:24:26 +0900

    release(heliotrope): bump up 5.3.5 to 6.0.0-alpha.0

--- heliotrope/__init__.py
+++ heliotrope/__init__.py
@@ -21,3 +21,17 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
+from typing import Literal, NamedTuple
+
+
+class VersionInfo(NamedTuple):
+    major: int
+    minor: int
+    micro: int
+    releaselevel: Literal["alpha", "beta", "candidate", "final"]
+    serial: int
+
+
+version_info = VersionInfo(major=6, minor=0, micro=0, releaselevel="alpha", serial=0)
+
+__version__ = f"{version_info.major}.{version_info.minor}.{version_info.micro}-{version_info.releaselevel}.{version_info.serial}"
