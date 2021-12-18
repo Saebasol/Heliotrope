1565a2781ccd2da62ab6b8e6a0f6c682148b9e1a
Ryu JuHeon <saidbysolo@gmail.com>
2021-12-18 12:29:00 +0900

    test(arg): add test code for arg

--- heliotrope/__main__.py
+++ heliotrope/__main__.py
@@ -1,93 +1,12 @@
-from argparse import ArgumentParser
+from sys import argv
 from json import loads
 from heliotrope.config import HeliotropeConfig
 from heliotrope.server import create_app
+from heliotrope.argparser import parse_args
 
 heliotrope_config = HeliotropeConfig()
 
-parser = ArgumentParser("heliotrope")
-
-parser.add_argument(
-    "--host",
-    type=str,
-    default="127.0.0.1",
-    help="The hostname to listen on (default: 127.0.0.1)",
-)
-parser.add_argument(
-    "--port",
-    type=int,
-    default=8000,
-    help="The port of the webserver (default: 8000)",
-)
-
-parser.add_argument(
-    "--workers",
-    type=int,
-    default=1,
-    help="The number of worker processes to spawn (default: 1)",
-)
-
-config = parser.add_argument_group("config")
-
-config.add_argument(
-    "--test",
-    action="store_true",
-    default=False,
-    help="Run the server in test mode (default: False)",
-)
-
-config.add_argument(
-    "--sentry-dsn",
-    type=str,
-    default="",
-    help="The Sentry DSN to use (default: '')",
-)
-
-config.add_argument(
-    "--galleryinfo-db-url",
-    type=str,
-    default="",
-    help="The url of the sql database (default: '')",
-)
-
-config.add_argument(
-    "--info-db-url",
-    type=str,
-    default="",
-    help="The url of the Meilisearch (default: '')",
-)
-
-config.add_argument(
-    "--index-file",
-    type=str,
-    default="index-korean.nozomi",
-    help="The index to use (default: index-korean.nozomi)",
-)
-
-
-config.add_argument(
-    "--mirroring-delay",
-    type=int,
-    default=3600,
-    help="The delay between mirroring task (default: 3600)",
-)
-
-config.add_argument(
-    "--refresh-delay",
-    type=str,
-    default=86400,
-    help="The delay between refresh commonjs (default: 86400)",
-)
-
-config.add_argument(
-    "--config",
-    type=str,
-    default="",
-    help="The path to the config file (default: '')",
-)
-
-
-args = parser.parse_args()
+args = parse_args(argv[1:])
 
 if args.config:
     with open(args.config, "r") as f:
---
+++ tests/test_arg.py
@@ -0,0 +1,48 @@
+from heliotrope.argparser import parse_args
+
+
+def test_parse_args():
+    args = parse_args(
+        [
+            "--host",
+            "127.0.0.1",
+            "--port",
+            "8000",
+            "--workers",
+            "1",
+            "--test",
+            "--sentry-dsn",
+            "",
+            "--galleryinfo-db-url",
+            "",
+            "--info-db-url",
+            "",
+            "--index-file",
+            "index-korean.nozomi",
+            "--mirroring-delay",
+            "0",
+            "--refresh-delay",
+            "0",
+        ]
+    )
+    assert args.host == ""
+    assert args.port == 8000
+    assert args.workers == 1
+    assert args.test == True
+    assert args.sentry_dsn == ""
+    assert args.galleryinfo_db_url == ""
+    assert args.info_db_url == ""
+    assert args.index_file == "index-korean.nozomi"
+    assert args.mirroring_delay == 0
+    assert args.refresh_delay == 0
+
+
+def test_parse_args_with_config():
+    args = parse_args(
+        [
+            "--config",
+            "tests/test_arg.json",
+        ]
+    )
+
+    assert args.config == "tests/test_arg.json"
