4d6e4e1459ceb01666991e05668bcade2d6a25a0
Ryu juheon <saidbysolo@gmail.com>
2021-12-30 21:59:05 +0900

    release(heliotrope): bump up 6.0.0-beta.1 to 6.0.0-beta.2 (#120)

--- heliotrope/__init__.py
+++ heliotrope/__init__.py
@@ -32,6 +32,6 @@ class VersionInfo(NamedTuple):
     serial: int
 
 
-version_info = VersionInfo(major=6, minor=0, micro=0, releaselevel="beta", serial=1)
+version_info = VersionInfo(major=6, minor=0, micro=0, releaselevel="beta", serial=2)
 
 __version__ = f"{version_info.major}.{version_info.minor}.{version_info.micro}-{version_info.releaselevel}.{version_info.serial}"
