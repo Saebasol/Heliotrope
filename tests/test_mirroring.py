c07d4b1aa2a22148d5b3e45a6f479a3fc94b81b7
Ryu JuHeon <saidbysolo@gmail.com>
2021-12-31 22:34:57 +0900

    test(mirroring): sleep 10 sec

--- tests/test_mirroring.py
+++ tests/test_mirroring.py
@@ -9,7 +9,7 @@ from heliotrope.tasks.mirroring import MirroringTask
 @mark.asyncio
 async def test_mirroring_task(fake_app: Heliotrope):
     await MirroringTask.setup(fake_app, 5)
-    await sleep(5)
+    await sleep(10)
     stats = await fake_app.ctx.meilisearch.index.get_stats()
     info_total = stats["numberOfDocuments"]
     galleryinfo_total = await fake_app.ctx.orm.get_all_index()
