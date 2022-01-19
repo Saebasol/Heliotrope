5dc064e75a6dfd18ef423413fc66b4a06cd68d60
Ryu juheon <saidbysolo@gmail.com>
2022-01-19 21:00:49 +0900

    refactor(interpreter): move function (#154)

--- heliotrope/interpreter/__init__.py
+++ heliotrope/interpreter/__init__.py
@@ -53,23 +53,11 @@ class CommonJS:
             # 폴리필을 사용하는 이유는 js2py에 없는 함수가 몇몇개 있기 때문이에요.
             polyfill = f.read()
         instance = cls(polyfill)
-        # If you continue to call the interpreter's function, it becomes very slow.
-        # Leave all the work to the interpreter.
-        # 계속해서 인터프리터의 함수를 호출을할경우 매우 느려집니다.
-        # 인터프리터에 모든작업을 맡깁니다.
-        instance.interpreter.execute(
-            """
-            function image_urls(galleryid, images, no_webp) {
-                return images.map(function (image) {
-                    var webp = null
-                    if (image.hash && image.haswebp && !no_webp){
-                        webp = 'webp'
-                    }
-                    return { 'name': image.name, 'url': url_from_url_from_hash(galleryid, image, webp) }
-                })
-            }
-            """
-        )
+        # See ./heliotrope/interpreter/function.js
+        # ./heliotrope/interpreter/function.js 파일 확인해주세요.
+        with open("./heliotrope/interpreter/function.js") as f:
+            function = f.read()
+        instance.interpreter.execute(function)
         instance.update_js_code(common_js_code, gg_js_code)
         return instance
 
---
+++ heliotrope/interpreter/function.js
@@ -0,0 +1,13 @@
+// If you continue to call the interpreter's function, it becomes very slow.
+// Leave all the work to the interpreter.
+// 계속해서 인터프리터의 함수를 호출을할경우 매우 느려집니다.
+// 인터프리터에 모든작업을 맡깁니다.
+function image_urls(galleryid, images, no_webp) {
+    return images.map(function (image) {
+        var webp = null
+        if (image.hash && image.haswebp && !no_webp) {
+            webp = 'webp'
+        }
+        return { 'name': image.name, 'url': url_from_url_from_hash(galleryid, image, webp) }
+    })
+}
