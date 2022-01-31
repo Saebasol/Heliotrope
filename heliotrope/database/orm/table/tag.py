12883de16038ec9f2075af9fa769018cb5faae6b
Ryu juheon <saidbysolo@gmail.com>
2022-01-31 12:16:27 +0900

    refactor(table): hitomi changes applied

--- heliotrope/database/orm/table/tag.py
+++ heliotrope/database/orm/table/tag.py
@@ -30,7 +30,7 @@ tag_table = Table(
     "tag",
     mapper_registry.metadata,
     Column("id", Integer, primary_key=True, autoincrement=True),
-    Column("index_id", Integer, ForeignKey("galleryinfo.id")),
+    Column("galleyinfo_id", Integer, ForeignKey("galleryinfo.id")),
     Column("male", String(1)),
     Column("female", String(1)),
     Column("tag", String, nullable=False),
