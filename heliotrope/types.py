c828596029484b6d207098253b83c5a0d79cfd2f
Ryu juheon <saidbysolo@gmail.com>
2022-01-31 12:16:39 +0900

    refactor(types): hitomi changes applied

--- heliotrope/types.py
+++ heliotrope/types.py
@@ -29,20 +29,20 @@ SetupTask = Task[NoReturn]
 
 class _HitomiFileJSONOptional(TypedDict, total=False):
     hasavifsmalltn: Literal[1]
-    hasavif: Literal[1]
 
 
 class HitomiFileJSON(_HitomiFileJSONOptional):
     width: int
     hash: str
     haswebp: Literal[0, 1]
+    hasavif: Literal[0, 1]
     name: str
     height: int
 
 
 class _HitomiTagJSONOptional(TypedDict, total=False):
-    male: Optional[Literal["", "1"]]
-    female: Optional[Literal["", "1"]]
+    male: Literal["", "1", 1]
+    female: Literal["", "1", 1]
 
 
 class HitomiTagJSON(_HitomiTagJSONOptional):
@@ -50,16 +50,60 @@ class HitomiTagJSON(_HitomiTagJSONOptional):
     tag: str
 
 
-class HitomiGalleryinfoJSON(TypedDict):
-    date: str
-    title: str
-    type: str
-    japanese_title: Optional[str]
-    language: Optional[str]
-    files: list[HitomiFileJSON]
-    id: Union[str, int]
+class HitomiParodysJSON(TypedDict):
+    parody: str
+    url: str
+
+
+class HitomiArtistsJSON(TypedDict):
+    artist: str
+    url: str
+
+
+class HitomiCharatersJSON(TypedDict):
+    character: str
+    url: str
+
+
+class HitomiGroupsJSON(TypedDict):
+    group: str
+    url: str
+
+
+class HitomiLanguagesJSON(TypedDict):
+    url: str
+    name: str
+    galleryid: str
     language_localname: str
-    tags: list[HitomiTagJSON]
+
+
+class HitomiGalleryinfoJSON(TypedDict):
+    # Union is for conversion.
+    id: Union[str, int]
+    # Literal["manga", "doujinshi", "gamecg", "aritstcg", "anime"]
+    type: str
+    # title
+    title: str
+    japanese_title: Optional[str]
+    # video
+    video: Optional[str]
+    videofilename: Optional[str]
+    # language
+    language_url: Optional[str]
+    language_localname: Optional[str]
+    language: Optional[str]
+    languages: list[HitomiLanguagesJSON]
+    # tags
+    artists: Optional[list[HitomiArtistsJSON]]
+    characters: Optional[list[HitomiCharatersJSON]]
+    parodys: Optional[list[HitomiParodysJSON]]
+    groups: Optional[list[HitomiGroupsJSON]]
+    files: list[HitomiFileJSON]
+    tags: Optional[list[HitomiTagJSON]]
+    # etc
+    scene_indexes: list[int]
+    related: list[int]
+    date: str
 
 
 class HitomiInfoJSON(TypedDict):
