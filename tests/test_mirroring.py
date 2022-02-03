39b14cc43b41c5902faf379f2b329b5ab81c67b0
Ryu juheon <saidbysolo@gmail.com>
2022-02-03 17:42:30 +0900

    test(mirroring): fix test

--- tests/test_mirroring.py
+++ tests/test_mirroring.py
@@ -7,15 +7,15 @@ from heliotrope.sanic import Heliotrope
 from heliotrope.tasks.mirroring import MirroringTask
 
 
+@mark.asyncio
 @mark.timeout(120)
 @mark.flaky(reruns=5, reruns_delay=5)
-def test_mirroring_task(fake_app: Heliotrope, event_loop: AbstractEventLoop):
+async def test_mirroring_task(fake_app: Heliotrope, event_loop: AbstractEventLoop):
     try:
-        event_loop.run_until_complete(wait_for(MirroringTask.setup(fake_app, 5), 15))
+        await wait_for(MirroringTask.setup(fake_app, 5), 15)
     except TimeoutError:
-        info_total = event_loop.run_until_complete(fake_app.ctx.odm.get_all_index())
-        galleryinfo_total = event_loop.run_until_complete(
-            fake_app.ctx.orm.get_all_index()
-        )
+        info_total = await fake_app.ctx.odm.get_all_index()
+        galleryinfo_total = await fake_app.ctx.orm.get_all_index()
+
         assert len(galleryinfo_total) >= 1
         assert len(info_total) >= 1
