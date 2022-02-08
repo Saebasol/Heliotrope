cb71c02f3ee0dbe99b53c0d37ca2f7f92f71f05c
Ryu juheon <saidbysolo@gmail.com>
2022-02-08 11:20:35 +0900

    release(heliotrope): bump up 6.0.0-candidate.0 to 6.0.0-candidate.1

--- heliotrope/__init__.py
+++ heliotrope/__init__.py
@@ -33,7 +33,7 @@ class VersionInfo(NamedTuple):
 
 
 version_info = VersionInfo(
-    major=6, minor=0, micro=0, releaselevel="candidate", serial=0
+    major=6, minor=0, micro=0, releaselevel="candidate", serial=1
 )
 
 __version__ = f"{version_info.major}.{version_info.minor}.{version_info.micro}-{version_info.releaselevel}.{version_info.serial}"
