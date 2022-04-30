9e743e21499f50de68ee3f83db6fcc2cecd64645
Ryu juheon <saidbysolo@gmail.com>
2022-04-30 11:05:16 +0900

    test: close the client after cleaning

--- tests/test_mirroring.py
+++ tests/test_mirroring.py
@@ -12,7 +12,7 @@ from heliotrope.tasks.mirroring import MirroringTask
 @mark.flaky(reruns=5, reruns_delay=5)
 async def test_mirroring_task(fake_app: Heliotrope, event_loop: AbstractEventLoop):
     try:
-        await wait_for(MirroringTask.setup(fake_app, 5), 15)
+        await wait_for(MirroringTask.setup(fake_app, 5), 5)
     except TimeoutError:
         info_total = await fake_app.ctx.odm.get_all_index()
         galleryinfo_total = await fake_app.ctx.orm.get_all_index()
