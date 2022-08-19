99fdffb05a92774813b18c12e1db71a25248c131
Ryu Juheon <saidbysolo@gmail.com>
2022-08-20 01:03:50 +0900

    release(heliotrope): bump up 6.2.7 to 6.3.4
    Signed-off-by: Ryu Juheon <saidbysolo@gmail.com>

--- heliotrope/__init__.py
+++ heliotrope/__init__.py
@@ -32,7 +32,7 @@ class VersionInfo(NamedTuple):
     serial: int
 
 
-version_info = VersionInfo(major=6, minor=2, micro=7, releaselevel="final", serial=0)
+version_info = VersionInfo(major=6, minor=3, micro=4, releaselevel="final", serial=0)
 
 __version__ = f"{version_info.major}.{version_info.minor}.{version_info.micro}"
 
