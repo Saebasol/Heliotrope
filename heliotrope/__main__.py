12431818ab2c23f4e81f39a41737c76bcc68ceed
Ryu juheon <saidbysolo@gmail.com>
2022-12-18 15:34:38 +0900

    fix: loader is not worked

--- heliotrope/__main__.py
+++ heliotrope/__main__.py
@@ -25,11 +25,7 @@ SOFTWARE.
 
 def main() -> None:  # pragma: no cover
     # I've done all of my testing on this.
-    from functools import partial
     from sys import argv
-
-    from sanic import Sanic
-    from sanic.worker.loader import AppLoader
 
     from heliotrope.argparser import parse_args
     from heliotrope.config import HeliotropeConfig
@@ -40,16 +36,13 @@ def main() -> None:  # pragma: no cover
     args = parse_args(argv[1:])
     heliotrope_config.update_with_args(args)
 
-    loader = AppLoader(factory=partial(create_app, heliotrope_config))
-    app = loader.load()
-
-    app.prepare(
+    app = create_app(heliotrope_config)
+    app.run(
         heliotrope_config.HOST,
         heliotrope_config.PORT,
         debug=heliotrope_config.DEBUG,
         workers=heliotrope_config.WORKERS,
     )
-    Sanic.serve(app, app_loader=loader)
 
 
 if __name__ == "__main__":  # pragma: no cover
