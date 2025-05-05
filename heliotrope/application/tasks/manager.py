76efe40e9b33b1f327b4060b2a5d5e015dc4d716
Ryu Juheon <saidbysolo@gmail.com>
2025-05-05 16:20:20 +0900

    feat: if task is done retry task

---
+++ heliotrope/application/tasks/manager.py
@@ -0,0 +1,32 @@
+from asyncio import Task
+from typing import Any, Callable, Coroutine, NoReturn
+from sanic.log import logger
+
+from heliotrope.infrastructure.sanic import Heliotrope
+
+
+def callback(
+    callback_task: Task[NoReturn],
+    retry_task: Callable[..., Coroutine[Any, Any, None]],
+    app: Heliotrope,
+) -> None:
+    logger.debug(f"Callback for {callback_task.get_name()}")
+
+    if callback_task.cancelled():
+        logger.warning(f"{callback_task.get_name()} Task was cancelled")
+    elif callback_task.exception():
+        try:
+            callback_task.result()
+        except:
+            logger.exception(f"{callback_task.get_name()} Task raised an exception")
+
+    logger.debug(f"{callback_task.get_name()} Task finished")
+    app.purge_tasks()
+    logger.debug(f"Purging {callback_task.get_name()} Task")
+    logger.info(f"Retrying {callback_task.get_name()} Task")
+    task = app.add_task(
+        retry_task(),
+        name=f"{callback_task.get_name()}",
+    )
+    assert task
+    task.add_done_callback(lambda t: callback(t, retry_task, app))
