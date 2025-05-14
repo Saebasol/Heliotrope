f142132f266a21b938ec4e1cf4354b7a94d7be8c
Ryu Juheon <saidbysolo@gmail.com>
2025-05-15 04:52:19 +0900

    style: add typed

--- heliotrope/infrastructure/sanic/error.py
+++ heliotrope/infrastructure/sanic/error.py
@@ -1,9 +1,9 @@
-from sanic import json
+from sanic import HTTPResponse, json
 
 from heliotrope.infrastructure.sanic.app import HeliotropeRequest
 
 
-async def not_found(request: HeliotropeRequest, exception: Exception):
+async def not_found(request: HeliotropeRequest, exception: Exception) -> HTTPResponse:
     return json(
         {
             "message": str(exception),
