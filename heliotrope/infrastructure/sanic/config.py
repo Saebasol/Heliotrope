e4ebd3f2e78ae092f09dd38d3204f0039249c0ee
Ryu Juheon <saidbysolo@gmail.com>
2025-05-21 02:58:35 +0900

    fix: fallback reponse

--- heliotrope/infrastructure/sanic/config.py
+++ heliotrope/infrastructure/sanic/config.py
@@ -46,7 +46,7 @@ class HeliotropeConfig(Config):
                 "DEBUG": False,
                 "ACCESS_LOG": False,
                 "FORWARDED_SECRET": "",
-                "FALLBACK_ERROR_FORMAT": "json",
+                "_FALLBACK_ERROR_FORMAT": "json",
                 # Sanic ext config
                 "OAS_UI_DEFAULT": "swagger",
                 "OAS_URI_REDOC": False,
