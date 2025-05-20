5e6fb2eca7f5d3ca1c0b82451c85d75f5bbdf71b
Ryu Juheon <saidbysolo@gmail.com>
2025-05-21 00:43:54 +0900

    release(7.0.0-beta.0): bump up version 7.0.0-alpha.5 to 7.0.0-beta.0
    Signed-off-by: Ryu Juheon <saidbysolo@gmail.com>

--- heliotrope/__init__.py
+++ heliotrope/__init__.py
@@ -9,7 +9,7 @@ class VersionInfo(NamedTuple):
     serial: int
 
 
-version_info = VersionInfo(major=7, minor=0, micro=0, releaselevel="alpha", serial=5)
+version_info = VersionInfo(major=7, minor=0, micro=0, releaselevel="beta", serial=0)
 
 __version__ = f"{version_info.major}.{version_info.minor}.{version_info.micro}"
 
