c37fc3f7b070ac10fd16afec7230656068e8abe7
Ryu Juheon <saidbysolo@gmail.com>
2025-05-22 11:19:48 +0900

    style: apply code style isort

--- migration/versions/45c39123b775_ondelete_cascade.py
+++ migration/versions/45c39123b775_ondelete_cascade.py
@@ -8,9 +8,8 @@ Create Date: 2025-05-22 10:26:15.176866
 
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "45c39123b775"
