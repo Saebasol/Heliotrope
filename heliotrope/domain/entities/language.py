988e7c383fec4b084956d7004458f94aff532c62
Ryu Juheon <saidbysolo@gmail.com>
2025-08-27 10:58:57 +0900

    fix: type in language schema

--- heliotrope/domain/entities/language.py
+++ heliotrope/domain/entities/language.py
@@ -2,12 +2,12 @@ from dataclasses import dataclass
 from typing import Optional
 
 from heliotrope.domain.base import HeliotropeEntity
-from heliotrope.domain.entities.language_localname import LangaugeLocalname
+from heliotrope.domain.entities.language_localname import LanguageLocalname
 
 
 @dataclass
 class Language(HeliotropeEntity):
     galleryid: Optional[int]
-    language_localname: LangaugeLocalname
+    language_localname: LanguageLocalname
     name: str
     url: str
