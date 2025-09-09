38d3b6779bebf96d0e332ad8454b00248b10327d
Ryu Juheon <saidbysolo@gmail.com>
2025-09-09 13:00:44 +0900

    fix: language field mapping in info entity (#553)

--- heliotrope/domain/entities/info.py
+++ heliotrope/domain/entities/info.py
@@ -67,5 +67,5 @@ class Info(HeliotropeEntity):
         if "series" in type_hints and "parodys" in galleryinfo_dict:
             info_dict["series"] = parse_tags_dict_list(galleryinfo_dict["parodys"])
         info_dict["thumbnail"] = File.from_dict(galleryinfo_dict["files"][0])
-        info_dict["language"] = galleryinfo_dict["language_localname"]["name"]
+        info_dict["language"] = galleryinfo_dict["language_info"]["language"]
         return cls(**info_dict)
