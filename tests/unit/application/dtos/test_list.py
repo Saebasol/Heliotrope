971dc87028d3c4d4afe2be666fbd7f1c75d74e6c
Ryu Juheon <saidbysolo@gmail.com>
2025-09-22 01:20:42 +0900

    test: add dtos test

---
+++ tests/unit/application/dtos/test_list.py
@@ -0,0 +1,14 @@
+from heliotrope.application.dtos.list import ListResultDTO
+from heliotrope.domain.entities.info import Info
+from tests.unit.domain.entities.conftest import sample_info as sample_info
+
+def test_list_result_dto_creation(sample_info: Info):
+    character = ListResultDTO(list=[sample_info], count=1)
+    assert character.list[0] == sample_info
+    assert character.count == 1
+
+
+def test_list_result_dto_serialization(sample_info: Info):
+    dto = ListResultDTO(list=[sample_info], count=1)
+    expected_dict = {"list": [sample_info.to_dict()], "count": 1}
+    assert dto.to_dict() == expected_dict
