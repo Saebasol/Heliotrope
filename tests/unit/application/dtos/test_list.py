23cf0c2b156565a825156fed4648035411f73713
Ryu Juheon <saidbysolo@gmail.com>
2025-09-30 13:32:06 +0900

    test: fix test

--- tests/unit/application/dtos/test_list.py
+++ tests/unit/application/dtos/test_list.py
@@ -4,12 +4,12 @@ from tests.unit.domain.entities.conftest import sample_info as sample_info
 
 
 def test_list_result_dto_creation(sample_info: Info):
-    character = ListResultDTO(list=[sample_info], count=1)
-    assert character.list[0] == sample_info
+    character = ListResultDTO(items=[sample_info], count=1)
+    assert character.items[0] == sample_info
     assert character.count == 1
 
 
 def test_list_result_dto_serialization(sample_info: Info):
-    dto = ListResultDTO(list=[sample_info], count=1)
-    expected_dict = {"list": [sample_info.to_dict()], "count": 1}
+    dto = ListResultDTO(items=[sample_info], count=1)
+    expected_dict = {"items": [sample_info.to_dict()], "count": 1}
     assert dto.to_dict() == expected_dict
