7eba67d39a959b76e801c30bbc5a4019f03a23d5
Ryu Juheon <saidbysolo@gmail.com>
2025-10-08 17:55:21 +0900

    test: rename test_about to test_root in e2e tests

--- tests/e2e/test_root.py
+++ tests/e2e/test_root.py
@@ -3,6 +3,6 @@ import pytest
 
 
 @pytest.mark.asyncio
-async def test_about(asgi_client: httpx.AsyncClient):
+async def test_root(asgi_client: httpx.AsyncClient):
     response = await asgi_client.get("/")
     assert response.status_code == 302
