de26091ef2bd5121b4b7600fa9461351470f5f5a
Ryu Juheon <saidbysolo@gmail.com>
2025-10-08 17:57:57 +0900

    feat: redesign about page (#602)
    * feat: redesign about page
    
    * test: async test for root endpoint redirect

--- heliotrope/adapters/endpoint/__init__.py
+++ heliotrope/adapters/endpoint/__init__.py
@@ -2,5 +2,6 @@ from sanic.blueprints import Blueprint
 
 from heliotrope.adapters.endpoint.about import about_endpoint
 from heliotrope.adapters.endpoint.api import api_endpoint
+from heliotrope.adapters.endpoint.root import root_endpoint
 
-endpoint = Blueprint.group(api_endpoint, about_endpoint)
+endpoint = Blueprint.group(api_endpoint, about_endpoint, root_endpoint)
