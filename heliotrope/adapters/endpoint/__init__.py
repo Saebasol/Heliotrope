d2d6f7d8c616aae589ae9929eb8f4becf09b0377
Ryu Juheon <saidbysolo@gmail.com>
2025-10-14 15:11:53 +0900

    fix: remove about endpoint import

--- heliotrope/adapters/endpoint/__init__.py
+++ heliotrope/adapters/endpoint/__init__.py
@@ -1,7 +1,6 @@
 from sanic.blueprints import Blueprint
 
-from heliotrope.adapters.endpoint.about import about_endpoint
 from heliotrope.adapters.endpoint.api import api_endpoint
 from heliotrope.adapters.endpoint.root import root_endpoint
 
-endpoint = Blueprint.group(api_endpoint, about_endpoint, root_endpoint)
+endpoint = Blueprint.group(api_endpoint, root_endpoint)
