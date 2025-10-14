6fd4f90f0b536676df359524ef3e6c09aad156af
Ryu Juheon <saidbysolo@gmail.com>
2025-10-14 15:09:58 +0900

    feat: remove about endpoint and items-info in html

--- heliotrope/adapters/endpoint/root.py
+++ heliotrope/adapters/endpoint/root.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from sanic import redirect
+from sanic import file
 from sanic.blueprints import Blueprint
 from sanic.response import HTTPResponse
 from sanic.views import HTTPMethodView
@@ -12,7 +12,10 @@ root_endpoint = Blueprint("root", url_prefix="/")
 
 class HeliotropeRootView(HTTPMethodView):
     async def get(self, request: HeliotropeRequest) -> Optional[HTTPResponse]:
-        return redirect("/about")
+        return await file(
+            "./heliotrope/adapters/endpoint/dashboard.html",
+            mime_type="text/html",
+        )
 
 
 root_endpoint.add_route(HeliotropeRootView.as_view(), "/")
