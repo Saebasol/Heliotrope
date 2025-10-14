3b9fb266ce8c776ef236d63626cd81f97425d5db
Ryu Juheon <saidbysolo@gmail.com>
2025-10-14 15:14:27 +0900

    test: fix test code

--- tests/e2e/test_about.py
+++
@@ -1,8 +0,0 @@
-import httpx
-import pytest
-
-
-@pytest.mark.asyncio
-async def test_about(asgi_client: httpx.AsyncClient):
-    response = await asgi_client.get("/about")
-    assert response.status_code == 200
--- tests/e2e/test_root.py
+++ tests/e2e/test_root.py
@@ -5,4 +5,4 @@ import pytest
 @pytest.mark.asyncio
 async def test_root(asgi_client: httpx.AsyncClient):
     response = await asgi_client.get("/")
-    assert response.status_code == 302
+    assert response.status_code == 200
